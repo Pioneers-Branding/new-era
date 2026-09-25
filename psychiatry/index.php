52c349b0c2286690a805c0e8c99036bdc9d0f663
Chitransh Saxena <chitransh.5055@gmail.com>
2026-09-25 13:42:42 +0530

    done

--- psychiatry/index.php
+++ psychiatry/index.php
@@ -456,7 +456,7 @@ tailwind.config = {
             </div>
 
             <form accept-charset="UTF-8" action="https://app.formester.com/forms/uaNJQHZaD/submissions" method="POST" class="mt-5 grid gap-4">
-              <input type="hidden" name="lead_form" value="1">
+              <input type="hidden" name="lead_form" value="2">
               <input type="hidden" name="lead_source" value="Psychiatry LP">
               <?php if ($formErr): ?>
                 <p class="rounded border border-red-300/40 bg-red-500/20 px-4 py-3 text-[13.5px] text-red-100"><?= htmlspecialchars($formErr) ?></p>
