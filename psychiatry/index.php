05507e0726fbde5c561d54b3f883d34cf2c1fb37
Chitransh Saxena <chitransh.5055@gmail.com>
2026-09-25 13:17:00 +0530

    done

--- psychiatry/index.php
+++ psychiatry/index.php
@@ -455,7 +455,7 @@ tailwind.config = {
               <span class="shrink-0 rounded border border-white/30 bg-white/15 px-2.5 py-1 text-[11px] font-semibold text-white tracking-wide">NO COST</span>
             </div>
 
-            <form accept-charset="UTF-8" action="https://app.formester.com/forms/uaNJQHZaD/submissions" method="POST" class="mt-5 grid gap-4">
+            <form accept-charset="UTF-8" action="https://app.formester.com/forms/H9XhrLPFP/submissions" method="POST" class="mt-5 grid gap-4">
               <input type="hidden" name="lead_form" value="1">
               <input type="hidden" name="lead_source" value="Psychiatry LP">
               <?php if ($formErr): ?>
